4b556d78353b0a02d09a9666d8e671c73c7c381b
wyy-holding <59436937+wyy-holding@users.noreply.github.com>
2025-10-27 16:18:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,9 @@
 # 设置带时间戳的版本号
 # shellcheck disable=SC2155
 export TAG=$(date +%Y%m%d%H%M%S)
-
+docker-compose down
 # 构建并启动容器
 docker-compose up -d --build
 
 # 清理旧镜像（保留最近的2个版本）
-docker images --filter=reference='ocr-server:*' --format "{{.Tag}}" | \
-grep -E '^[0-9]{14}$' | \
-sort -r | \
-awk -v keep=2 'NR > keep' | \
-xargs -I {} docker rmi flet-server:{} 2>/dev/null
+docker rmi $(docker images 'ocr-server' --format '{{.ID}}' | tail -n +3)
